02c325f33c9829bf531d31973b12f45a9878006a
suot00 <truongkhacsuot661@gmail.com>
2020-10-01 15:32:31 +0700

    login firebase

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -27,38 +27,38 @@ export default function LoginScreen({navigation}) {
     }
   };
   //
-  useEffect(() => {
-    GoogleSignin.configure({
-      scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
-      webClientId:
-        '597818321076-saj2e1ad29b8kgegkfkib7vjnk04lnmo.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
-      offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
-      // hostedDomain: '', // specifies a hosted domain restriction
-      // loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
-      forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
-      // accountName: '', // [Android] specifies an account name on the device that should be used
-      // iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
-    });
-  });
+  // useEffect(() => {
+  //   GoogleSignin.configure({
+  //     scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+  //     webClientId:
+  //       '597818321076-saj2e1ad29b8kgegkfkib7vjnk04lnmo.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+  //     offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+  //     // hostedDomain: '', // specifies a hosted domain restriction
+  //     // loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
+  //     forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+  //     // accountName: '', // [Android] specifies an account name on the device that should be used
+  //     // iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+  //   });
+  // });
 
-  const signIn = async () => {
-    try {
-      await GoogleSignin.hasPlayServices();
-      const userInfo = await GoogleSignin.signIn();
-      this.setState({userInfo});
-      console.log(userInfo);
-    } catch (error) {
-      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-        // user cancelled the login flow
-      } else if (error.code === statusCodes.IN_PROGRESS) {
-        // operation (e.g. sign in) is in progress already
-      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-        // play services not available or outdated
-      } else {
-        // some other error happened
-      }
-    }
-  };
+  // const signIn = async () => {
+  //   try {
+  //     await GoogleSignin.hasPlayServices();
+  //     const userInfo = await GoogleSignin.signIn();
+  //     this.setState({userInfo});
+  //     console.log(userInfo);
+  //   } catch (error) {
+  //     if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+  //       // user cancelled the login flow
+  //     } else if (error.code === statusCodes.IN_PROGRESS) {
+  //       // operation (e.g. sign in) is in progress already
+  //     } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+  //       // play services not available or outdated
+  //     } else {
+  //       // some other error happened
+  //     }
+  //   }
+  // };
 
   return (
     <View style={styles.container}>
@@ -86,11 +86,7 @@ export default function LoginScreen({navigation}) {
           />
         </View>
         <View style={styles.subContainer}>
-          <Button
-            style={styles.textInput}
-            title="Login"
-            onPress={() => login()}
-          />
+          <Button style={{width: 50}} title="Login" onPress={() => login()} />
         </View>
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
           <Text
@@ -108,14 +104,13 @@ export default function LoginScreen({navigation}) {
             Don't have account , create now
           </Text>
         </View>
-        <View>
+        {/* <View style={{alignItems: 'center', justifyContent: 'center'}}>
           <GoogleSigninButton
-            style={{width: 192, height: 48}}
             size={GoogleSigninButton.Size.Wide}
             color={GoogleSigninButton.Color.Dark}
             onPress={() => signIn()}
           />
-        </View>
+        </View> */}
         {showLoading && (
           <View style={styles.activity}>
             <ActivityIndicator size="large" color="#0000ff" />
